9dc41784b39ae0cb7d8760000634c9abf9748f36
greggagne <ggagne@westminsteru.edu>
2023-08-24 18:37:51 -0600

    Initial Commit

---
+++ src/CoPrimes.java
@@ -0,0 +1,26 @@
+/**
+ * Generate the co-primes from (1,1) to (m,n)
+ * 
+ * [YOUR NAME GOES HERE]
+ *
+ */
+public class CoPrimes {
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.err.println("Must be passed two integer values > 0");
+			
+			System.exit(0);
+		}
+		
+		int m = Integer.parseInt(args[0]);
+		int n = Integer.parseInt(args[1]);
+		
+		/**
+		 * Provide the necessary logic to generate the co-prime 
+		 * pairs from (1,1) to (m,n).
+		 * 
+		 * This will likely involve additional helper method
+		 */
+	}
+
+}
